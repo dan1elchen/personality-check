d538e9f6b2882b89a7d29d5e44cff0bf05576f1a
fionamei131@gmail.com <fionamei131@gmail.com>
2022-10-01 21:00:27 -0400

    removed comments

--- backend/reddit.py
+++ backend/reddit.py
@@ -11,19 +11,12 @@ user_agent = os.getenv("REDDIT_USER_AGENT")
 reddit_read_only = praw.Reddit(client_id=client_id, 
                                 client_secret=client_secret,
                                 user_agent=user_agent)
-url="https://www.reddit.com/r/BostonU/comments/xsvq2a/finding_your_people/"
 
 def getRedditInfo(url):
     submission = reddit_read_only.submission(url=url)
     redditInfo = {"title":submission.title, "selftext":submission.selftext, "comments":[]}
 
-    # print("post text:", submission.title, "\n")
-    # print("self text:", submission.selftext, "\n")
-
     for comment in submission.comments:
         redditInfo["comments"].append(comment.body)
-        # print("comments:", comment.body, "\n")
-    # print("end")
     return redditInfo
 
-print(getRedditInfo(url))
