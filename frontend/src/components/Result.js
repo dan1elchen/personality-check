e8e3cd2c66390c94897fb33235eee87ba62078d6
dan1elchen <dfc@bu.edu>
2022-10-02 10:43:34 -0400

    frontend changes

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -1,19 +1,37 @@
 import "../css/Result.css";
-import React from "react";
+import { React, useEffect, useState } from "react";
 import Navbar from "./Navbar";
+import axios from "axios";
 
 function Result() {
+  const [anaysis, setAnalysis] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios({
+      method: "GET",
+    });
+  };
   return (
     <div class="Result">
       <Navbar />
-      <div class="Profile">
+      <div class="output-header">
         {/* <img src="default.jpg"></img> */}
         <p class="username">Daniel Chen</p>
       </div>
-      <div class="output">
-        <p>ISTJ</p>
-        <p>96% MATCH</p>
+      <div class="description info">
         <p>Description</p>
+      </div>
+      <div class="emojis">
+        {/* <img
+          src={require("../assets/positive.jpeg")}
+          width="100px"
+          height="100px"
+        /> */}
+      </div>
+      <div class="info">
+        <div class="text-info">0% Positive</div>
+        <div class="text-info"> 72% Neutral </div>
+        <div class="text-info">28% Negative</div>
       </div>
     </div>
   );
